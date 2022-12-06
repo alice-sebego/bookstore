9f94d044198bf0f21b28741e177c683f2dde4cf0
Alice Sebego <alice.etvotrecom@gmail.com>
2022-12-06 15:59:11 +0100

    chore: add comments for Author gettersand setters

--- Author.php
+++ Author.php
@@ -16,46 +16,74 @@ class Author{
       $this->books = [];
     }
 
+    /**
+     * Get the value of firstname
+     */ 
     public function getFirstname(){
         return $this->firstname;
     }
 
+    /**
+     * Set the value of firstname
+     * @return self
+     */
     public function setFirstname($firstname){
         $this->firstname = $firstname;
+        return $this;
     }
 
+    /**
+     * Get the value of lastname
+     */
     public function getLastname(){
         return $this->lastname;
     }
 
+    /**
+     * Set the value of lastname
+     * @return self
+     */
     public function setLastname($lastname){
         $this->lastname = $lastname;
+        return $this;
     }
 
+    /**
+     * Get the value of gender
+     */
     public function getGender(){
         return $this->gender;
     }
 
+    /**
+     * Set the value of gender
+     * @return self
+     */
     public function setGender($gender){
         $this->gender = $gender;
+        return $this;
     }
 
+    /**
+     * Get the value of birthday
+     */
     public function getBirthday(){
         return $this->birthday;
     }
 
+    /**
+     * Set the value of birthday
+     * @return self
+     */
     public function setBirthday($birthday){
         $this->birthday = $birthday;
+        return $this;
     }
 
     public function displayBooks(){
         $allBooks = implode(", ", $this->books);
         return $allBooks;
     }
-
-    // public function setBooks($books){
-    //     $this->books = $books;
-    // }
 
     public function getAge(){
         // Soluc' Zamina    
